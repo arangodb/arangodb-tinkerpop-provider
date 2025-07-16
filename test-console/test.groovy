6d3c088df1c3a471a06ebd0508f55077d39b7ac3
Michele Rastelli <michele@arangodb.com>
2025-07-16 14:36:25 +0200

    test console plugin

---
+++ test-console/test.groovy
@@ -0,0 +1,16 @@
+graph = GraphFactory.open("conf/arangodb.yaml")
+graph.io(IoCore.graphson()).readGraph("data/tinkerpop-modern.json")
+g = graph.traversal()
+res = g.V().as("a").out("knows").as("b").select("a", "b").by("name").toList()
+try {
+    assert res.stream().anyMatch(x ->
+            x.get("a").equals("marko") && x.get("b").equals("vadas"))
+    assert res.stream().anyMatch(x ->
+            x.get("a").equals("marko") && x.get("b").equals("josh"))
+} catch (AssertionError e) {
+    // required to exit(1): gremlin console exits 1 only in case of Exception, not Error
+    throw new RuntimeException(e)
+} finally {
+    graph.close()
+}
+println("DONE")
