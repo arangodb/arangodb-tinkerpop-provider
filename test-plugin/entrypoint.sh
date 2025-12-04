b3b973ed570e4755fbacf25b7d704a94fc488623
Michele Rastelli <michele@arangodb.com>
2025-12-04 15:47:36 +0100

    v3.3.0

--- test-plugin/entrypoint.sh
+++ test-plugin/entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-/opt/gremlin-server/bin/gremlin-server.sh install com.arangodb arangodb-tinkerpop-provider 3.2.1
+/opt/gremlin-server/bin/gremlin-server.sh install com.arangodb arangodb-tinkerpop-provider 3.3.0
 /opt/gremlin-server/bin/gremlin-server.sh /arangodb/gremlin-server-arangodb.yaml
